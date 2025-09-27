691a0a490183fb3e04d1906e243336a72abb1a11
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-09-27 20:51:48 +0700

    Метод для определения типа команды

--- Assembler/Command.cs
+++ Assembler/Command.cs
@@ -8,6 +8,7 @@ namespace FactorioComputerSimulator.Assembler
         public abstract string Name { get; }
         public abstract int Id { get; }
 
+        public abstract int GetCommandType(string[] command);
         public abstract int GetByteData(int commandType);
         public abstract void Execute(ref int pc, int commandType, byte[] args, Registers registers, Memory ram);
     }
