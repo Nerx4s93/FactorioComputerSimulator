d66ce74b2ddeb40acb9bfa7dc510f3217ca43006
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-09-30 20:56:40 +0700

    Увеличение размера ПЗУ и ОЗУ

--- Assembler/CodeSimulator.cs
+++ Assembler/CodeSimulator.cs
@@ -6,6 +6,6 @@ internal class CodeSimulator
 {
     public SimulatorHandle StartSimulation(CompiledLine[] code)
     {
-        return new SimulatorHandle(code, 512, 64);
+        return new SimulatorHandle(code, 1024, 256);
     }
 }
