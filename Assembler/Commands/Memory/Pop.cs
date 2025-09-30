a9a480f107fd5f359bce0c4bc7267c8a48d6aeb6
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-09-30 13:37:39 +0700

    Команды push pop, TODO реализация

---
+++ Assembler/Commands/Memory/Pop.cs
@@ -0,0 +1,43 @@
+﻿using FactorioComputerSimulator.Assembler.Exceptions;
+using FactorioComputerSimulator.Assembler.ParsingChecks;
+using FactorioComputerSimulator.Assembler.Simulation;
+
+namespace FactorioComputerSimulator.Assembler.Commands.Memory;
+
+internal class Pop : Command
+{
+    public override string Group => "Memory";
+    public override string Name => "pop";
+    public override int Id => 21;
+
+    public override int GetCommandType(string[] command)
+    {
+        var registerCheck = new RegisterCheck();
+        if (registerCheck.Check(command[0]))
+        {
+            return 0;
+        }
+
+        return -1;
+    }
+
+    public override int GetByteData(int commandType)
+    {
+        // 00: reg <- stek | pop A
+
+        switch (commandType)
+        {
+            case 0:
+                {
+                    return 1;
+                }
+        }
+
+        throw new InvalidCommandTypeException(Name, commandType);
+    }
+
+    public override void Execute(ref int pc, int commandType, byte[] args, Registers registers, Simulation.Memory ram)
+    {
+        pc += 2 + GetByteData(commandType);
+    }
+}
---
+++ Assembler/Commands/Memory/Push.cs
@@ -0,0 +1,49 @@
+﻿using FactorioComputerSimulator.Assembler.Exceptions;
+using FactorioComputerSimulator.Assembler.ParsingChecks;
+using FactorioComputerSimulator.Assembler.Simulation;
+
+namespace FactorioComputerSimulator.Assembler.Commands.Memory;
+
+internal class Push : Command
+{
+    public override string Group => "Memory";
+    public override string Name => "push";
+    public override int Id => 21;
+
+    public override int GetCommandType(string[] command)
+    {
+        var registerCheck = new RegisterCheck();
+        if (registerCheck.Check(command[0]))
+        {
+            return 1;
+        }
+        else if (command.Length == 2)
+        {
+            return 0;
+        }
+
+        return -1;
+    }
+
+    public override int GetByteData(int commandType)
+    {
+        // 00: const -> stek | push 9
+        // 01: reg -> stek   | push A
+
+        switch (commandType)
+        {
+            case 0:
+            case 1:
+                {
+                    return 1;
+                }
+        }
+
+        throw new InvalidCommandTypeException(Name, commandType);
+    }
+
+    public override void Execute(ref int pc, int commandType, byte[] args, Registers registers, Simulation.Memory ram)
+    {
+        pc += 2 + GetByteData(commandType);
+    }
+}
