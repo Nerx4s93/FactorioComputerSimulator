1a1f7c1f03a6bdd476fdfe9d659ced8ab6d9d17a
Nerx4s93 <nikitahysnutdinov@gmail.com>
2025-09-30 14:22:26 +0700

    Изменение ригстров

--- Assembler/Simulation/Registers.cs
+++ Assembler/Simulation/Registers.cs
@@ -31,7 +31,7 @@ public class Registers
     private static readonly string[] _registerNamesById = new string[]
         {
             "A", "L", "M", "H", "K", "J", "C",
-            "S", "Ss1", "Ss2", "Sc",
+            "Sba", "Sbb", "Spa", "Spb", "Sc",
             "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7"
         };
 
